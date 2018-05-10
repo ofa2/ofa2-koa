c5f155e28b3557044ef26baeeb72fbe6c5adf054
xinshangshangxin <xinshangshangxin@gmail.com>
2018-05-10 16:19:04 +0800

    chore: eslint && uglify

--- dist/bundle.cjs.js
+++ dist/bundle.cjs.js
@@ -1,13 +1,2 @@
-'use strict';
-
-function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
-
-var Koa = _interopDefault(require('koa'));
-
-function index () {
-  this.Koa = Koa;
-  this.app = new Koa();
-}
-
-module.exports = index;
+"use strict";function _interopDefault(e){return e&&"object"==typeof e&&"default"in e?e.default:e}var Koa=_interopDefault(require("koa"));function index(){this.Koa=Koa,this.app=new Koa}module.exports=index;
 //# sourceMappingURL=bundle.cjs.js.map
--- dist/bundle.esm.js
+++ dist/bundle.esm.js
@@ -1,9 +1,2 @@
-import Koa from 'koa';
-
-function index () {
-  this.Koa = Koa;
-  this.app = new Koa();
-}
-
-export default index;
+import Koa from"koa";function index(){this.Koa=Koa,this.app=new Koa}export default index;
 //# sourceMappingURL=bundle.esm.js.map
